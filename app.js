358997136f61c13f383cc5eb294660ca5f103701
OluwatomisinAtamamen <47326823+OluwatomisinAtamamen@users.noreply.github.com>
2024-02-27 22:06:49 +0000

    Completed all Objectives

--- app.js
+++ app.js
@@ -6,14 +6,25 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+  try {
+    const stockSymbols = await stocks.getStocks()
+    res.send({ stockSymbols })
+  } catch (error) {
+    console.log('Unable to get stock symbols');
+    res.status(500).send('Unable to get stock symbols');
+  }
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
   const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
+  try {
+    const data = await stocks.getStockPoints(symbol, new Date())
+    res.send(data)
+    console.log(`Got stock data for ${symbol}`);
+  } catch (error) {
+    console.log(`Unable to get stock data for ${symbol}`);
+    res.status(500).send(`Unable to get stock data for ${symbol}`);
+  }
 })
 
 app.listen(3000, () => console.log('Server is running!'))
--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,4 @@
-const canvas = document.getElementById('chart')
-const ctx = canvas.getContext('2d')
-
-function drawLine (start, end, style) {
+function drawLine (ctx, start, end, style) {
   ctx.beginPath()
   ctx.strokeStyle = style || 'black'
   ctx.moveTo(...start)
@@ -9,16 +6,75 @@ function drawLine (start, end, style) {
   ctx.stroke()
 }
 
-function drawTriangle (apex1, apex2, apex3) {
+function drawTriangle (ctx, apex1, apex2, apex3) {
   ctx.beginPath()
   ctx.moveTo(...apex1)
   ctx.lineTo(...apex2)
   ctx.lineTo(...apex3)
   ctx.fill()
 }
+// object to store all global variables
+const globalObj = {};
 
-drawLine([50, 50], [50, 550])
-drawTriangle([35, 50], [65, 50], [50, 35])
+// function to view stocks
+async function viewStocks(stocks) {
+  globalObj.structure = {};
 
-drawLine([50, 550], [950, 550])
-drawTriangle([950, 535], [950, 565], [965, 550])
+  for (let stockSymbol of stocks) {
+    // store stock data in globalObj.structure
+    globalObj.structure[stockSymbol] = await getStockData(stockSymbol);
+
+    // creates a list element for each stock
+    const symbol = document.createElement('li');
+    symbol.textContent = stockSymbol;
+    globalObj.stocksClass.append(symbol);
+
+    // creates a canvas for each stock
+    const canvas = document.createElement('canvas');
+    canvas.width = 1000;
+    canvas.height = 600;
+    globalObj.stocksClass.append(canvas)
+
+    const ctx = canvas.getContext('2d')
+
+    drawLine(ctx, [50, 50], [50, 550])
+    drawTriangle(ctx, [35, 50], [65, 50], [50, 35])
+
+    drawLine(ctx, [50, 550], [950, 550])
+    drawTriangle(ctx, [950, 535], [950, 565], [965, 550])
+
+  }
+  console.log(globalObj.structure);
+
+  // hides the spinner
+  globalObj.spinner.classList.add('hide');
+}
+
+// function to get stock list
+async function getStockList(){
+  const response = await fetch('stocks');
+  let stocks;
+  if (response.ok) {
+    stocks = await response.json();
+  } else {
+    stocks = [{ error: 'failed to load stocks :-(' }];
+  }
+  viewStocks(stocks.stockSymbols);
+}
+
+// function to get each stock data
+async function getStockData(symbol){
+  const response = await fetch('stocks/' + symbol);
+  let data;
+  if (response.ok) {
+    data = await response.json();
+  } else {
+    data = [{ error: 'failed to load stock data :-(' }];
+  }
+  return data;
+}
+
+globalObj.spinner = document.querySelector('.spinner');
+globalObj.stocksClass = document.querySelector('.stocks');
+
+window.addEventListener('load', getStockList);
